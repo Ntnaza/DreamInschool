debe25558d284f184023bcd1b8be7f9aba7743ba
Ntnaza <masadepanku1712@gmail.com>
2026-04-20 22:13:53 +0700

    aaa

--- server.js
+++ server.js
@@ -3,24 +3,20 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
 const port = process.env.PORT || 3000
-// Initialize Next.js
-const app = next({ dev, hostname, port })
+
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true)
-      await handle(req, res, parsedUrl)
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('Internal Server Error')
-    }
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
+    console.log(`> Ready on port ${port}`)
   })
+}).catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
 })
